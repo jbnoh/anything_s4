05aadfe8262881f6676ffac72ed2a73952c3f592
rjb <>
2022-09-17 12:36:15 +0900

    init

---
+++ src/test/java/com/anything/AnythingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnythingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
