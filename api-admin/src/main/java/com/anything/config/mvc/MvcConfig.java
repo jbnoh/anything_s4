25e4a6820455ee18563871153068f949bd877770
nohjb92 <nohjb92@gmail.com>
2022-11-17 00:12:19 +0900

    CORS 필터 클래스 별도 생성

--- api-admin/src/main/java/com/anything/config/mvc/MvcConfig.java
+++ api-admin/src/main/java/com/anything/config/mvc/MvcConfig.java
@@ -2,12 +2,10 @@ package com.anything.config.mvc;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.anything.interceptor.AuthInterceptor;
-import com.anything.type.DefaultType.JwtType;
 import com.anything.type.converter.StringTypeParameterConverter;
 
 import lombok.RequiredArgsConstructor;
@@ -34,16 +32,6 @@ public class MvcConfig implements WebMvcConfigurer {
 		registry.addInterceptor(authInterceptor)
 				.excludePathPatterns(excludeSystem)
 				.excludePathPatterns("/api/v1/login/**");
-	}
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-
-		registry.addMapping("/**")
-				.allowedOrigins("*")
-				.allowedMethods("GET", "POST", "PUT", "DELETE")
-				.allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization", JwtType.TOKEN.getValue())
-				.maxAge(3600);
 	}
 
 	@Override
